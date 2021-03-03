f81915109ca8e09dd3fce3a61df2751458bd65bd
Carlos Escorcia <cmescorcia5@misena.edu.co>
2021-03-02 20:11:41 -0500

    Punto 3

---
+++ punto3.py
@@ -0,0 +1,14 @@
+valorFianza = float(input('Digite el monto de la fianza: '))
+if valorFianza > 0:
+    if valorFianza > 50000:
+        cuota = (0.02 * valorFianza)
+        print(f'Su compra aplica para una cuota del: 2%')
+        print(f'Valor de la fianza: {valorFianza}')
+        print(f'Valor de la cuota: {cuota}')
+    else:
+        cuota = (0.03 * valorFianza)
+        print(f'Su compra aplica para una cuota del: 3%')
+        print(f'Valor de la fianza: {valorFianza}')
+        print(f'Valor de la cuota: {cuota}')
+else:
+    print(f'El valor de la fianza debe ser mayor a 0')
